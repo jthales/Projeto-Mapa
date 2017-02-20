bef4e3900b26aef7a5619a70d70c5cb1b5f61bd1
jthales <thaleshenrique38@live.com>
2017-02-20 20:58:12 -0300

    Mapa do Ba
    ADD Arquivos

---
+++ README.md
@@ -0,0 +1,19 @@
+# Projeto Mapa do Bairro
+## Udacity Desenvolvimento Front-End Projeto 09
+
+### Visão Geral
+Este Projeto usa Javascript, jQuery, knockoutJS, API do Google Maps e API do Yelp para criar um mapa dos locais para entretenimento em São Roque-SP e o Ratings dos locais, e também uma folha de estilos do Bootstrap.
+
+#### Projeto precisou de:
+Este Projeto requisita Grunt para executar o Gruntfile. Além disso o Bower e o pacote oath-signature que também são usados.
+
+#### Localizações
+Todo código fonte (desenvolvimento) está localizado no diretório directbairros.
+
+## Usando o APP
+
+#### Usando recurso pesquisa
+Os usuários podem filtrar os locais com base no nome do local, do bairro que está situado ou até mesmo do CEP; Apenas digitar o que será pesquisado e clicar em pesquisar que será exibido o resultado da pesquisa.
+
+# Créditos
+Desenvolvimento foi feito todo por mim com algumas pesquisas via stackoverflow de todos códigos e os API's que ajudariam a melhorar o projeto.
---
+++ directbairros/js/model.js
@@ -0,0 +1,149 @@
+var breweryLocations = [
+  {
+    name: 'Ski Mountain Park',
+    lat: -23.506379,
+    lng: -47.118271,
+    address: 'Av 3 de Maio, s/n° - Cambará, São Roque, SP 18133-310',
+    neighborhood: 'Cambará'
+  },
+  {
+    name: 'Clube Come Together',
+    lat: -23.532942,
+    lng: -47.133783,
+	address: 'Rua Marechal Deodoro da Fonseca 123-Centro, São Roque, SP 18136-000',
+    neighborhood: 'Centro'
+  },
+  {
+    name: 'Bar do Nelson',
+    lat: -23.542647,
+    lng: -47.126775,
+    address: 'R São Paulo 1204 - Taboão, São Roque, SP 18130-070',
+    neighborhood: 'Taboão'
+  },
+  {
+    name: 'Vinícola Góes',
+    lat: -23.611149,
+    lng: -47.160335,
+    address: 'Km 9 s/n° - Canguera, São Roque, SP 18145-002',
+    neighborhood: 'Canguera'
+  },
+  {
+    name: 'Stefano Hotel e Restaurante',
+    lat: -23.551064,
+    lng: -47.101483,
+    address: 'Av. Antonio Pannellini 2650 - Taboão, São Roque, SP 18135-131',
+    neighborhood: 'Taboão'
+  },
+  {
+    name: 'São Roque Clube',
+    lat: -23.530302,
+    lng: -47.136847,
+    address: 'Av Antonino Dias Bastos 368 - Centro, São Roque, SP 18130-350',
+    neighborhood: 'Centro'
+  },
+  {
+    name: 'Hotel Cordialle',
+    lat: -23.534937,
+    lng: -47.137435,
+    address: 'R Sotero de Souza 500 - Centro, São Roque, SP 18130-200',
+    neighborhood: 'Centro'
+  },
+  {
+    name: 'Morro do Cruzeiro',
+    lat: -23.537625,
+    lng: -47.143170,
+    address: 'R do Cruzeiro s/n° - Jardim Suiça, São Roque, SP 18130-000',
+    neighborhood: 'Jd Suiça'
+  },
+  {
+    name: 'V8 Restaurante & Pizzzaria',
+    lat: -23.527720,
+    lng: -47.137283,
+    address: 'Av Antonino Dias Bastos 699 - Centro, São Roque, SP 18130-350',
+    neighborhood: 'Centro'
+  },
+  {
+    name: 'Santa Costela',
+    lat: -23.533171,
+    lng: -47.136785,
+    address: 'Av Antonino Dias Bastos 37 - Centro, São Roque, SP 18130-350',
+    neighborhood: 'Centro'
+  },
+  {
+    name: 'Quinta do Olivardo',
+    lat: -23.576726,
+    lng: -47.139200,
+    address: 'Estrada do Vinho s/n° - Sorocamirim, São Roque, SP 18145-002',
+    neighborhood: 'Sorocamirim'
+  },
+  {
+    name: 'Cantineiro Bar',
+    lat: -23.520875,
+    lng: -47.138562,
+    address: 'R V e Dois - Centro, São Roque, SP 18132-305',
+    neighborhood: 'Centro'
+  },
+  {
+    name: 'O Padroeiro',
+    lat: -23.524818,
+    lng: -47.133363,
+    address: 'Av Antonino Dias Bastos 2-124 - Centro, São Roque, SP 18130-350',
+    neighborhood: 'Centro'
+  },
+  {
+    name: 'Villa Don Patto',
+    lat: -23.563124,
+    lng: -47.134718,
+    address: 'Estr do Vinho s/n° - Sorocamirim, São Roque, SP 18136-280',
+    neighborhood: 'Sorocamirim'
+  },
+  {
+    name: 'Fazendinha e Reestaurante Santa Adélia',
+    lat: -23.611320,
+    lng: -47.164315,
+    address: 'R Benedito Silvino de Camargo 400 - Canguera, São Roque, SP 18145-040',
+    neighborhood: 'Canguera'
+  },
+  {
+    name: 'Vinícola Palmeiras',
+    lat: -23.612101,
+    lng: -47.156547,
+    address: 'Estrada do Vinho KM10 s/n° - Canguera, São Roque, SP 18130-000',
+    neighborhood: 'Canguera'
+  },
+  {
+    name: 'Cantina da Tia Lina',
+    lat: -23.612497,
+    lng: -47.155533,
+    address: 'Estrada do Vinho KM10,4 s/n° - Canguera, São Roque, SP 18145-002',
+    neighborhood: 'Canguera'
+  },
+  {
+    name: 'Vinhos e Restaurante Frank',
+    lat: -23.550045,
+    lng: -47.127003,
+    address: 'Av. Madre Silva 494 - Vila Amaral, São Roque, SP 18136-280',
+    neighborhood: 'Vila Amaral'
+  },
+  {
+    name: 'Rancho do Netão',
+    lat: -23.554431,
+    lng: -47.125465,
+    address: 'Rua Olavo Capuzzo 180 - Taboão, São Roque, SP 18135-139',
+    neighborhood: 'Taboão'
+  },
+  {
+    name: 'Churrascaria Chama Sul',
+    lat: -23.555491,
+    lng: -47.113359,
+    address: 'Rod Raposo Tavares 448 - Jd Mosteiro, São Roque, SP 18130-000',
+    neighborhood: 'Jardim Mosteiro'
+  },
+  {
+    name: 'Vinícola Canguera',
+    lat: -23.601118,
+    lng: -47.162751,
+    address: 'Rodovia Prefeito Quintino de Lima - Canguera, São Roque, SP 18130-000',
+    neighborhood: 'Canguera'
+  }
+];
