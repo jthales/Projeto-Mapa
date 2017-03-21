dc40e009466985892e245f4d24c5ba430a32f9de
jthales <thaleshenrique38@live.com>
2017-03-20 21:23:03 -0300

    ATT

--- directbairros/js/app.js
+++ directbairros/js/app.js
@@ -1,170 +1,172 @@
 // Inicializa Mapa
 var map;
+var infoWindow;
 
-var infoWindow = new google.maps.InfoWindow({
-  content: '<div><h4 id="brewery-name"></h4><p id="brewery-address"></p><p id="yelp"></p></div>'
-});
+var ViewModel = function () {
+        'use strict';
+        var vm = new ViewModel();
+        ko.applyBindings(vm);
+        var self = this;
+        self.breweryList = ko.observableArray([]);
+        self.filteredBreweryList = ko.observableArray([]);
 
-var ViewModel = function() {
-  'use strict';
+        // Inicio onde com zoom
+        self.initialize = function () {
+            var mapCanvas = document.getElementById('google-map');
+            var cenLatLng = new google.maps.LatLng(-23.5308, -47.1355);
+            var mapOptions = {
+                center: cenLatLng,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            map = new google.maps.Map(mapCanvas, mapOptions);
+            infoWindow = new google.maps.InfoWindow({
+                content: '<div><h4 id="brewery-name"></h4><p id="brewery-address"></p><p id="yelp"></p></div>'
+            });
 
-  var self = this;
-  self.breweryList = ko.observableArray([]);
-  self.filteredBreweryList = ko.observableArray([]);
+            self.initialize();
+            self.buildBreweryLocations();
+            self.setBreweryClickFunctions();
+            self.filteredBreweryList(self.breweryList());
+        };
+	
+        self.buildBreweryLocations = function () {
+            breweryLocations.forEach(function (brewItem) {
+                self.breweryList.push(new Brewery(brewItem));
+            });
+        };
 
-  // Inicio onde com zoom
-  self.initialize = function() {
-    var mapCanvas = document.getElementById('google-map');
-    var cenLatLng = new google.maps.LatLng(-23.5308, -47.1355);
-    var mapOptions = {
-      center: cenLatLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+        self.setBreweryClickFunctions = function () {
+            self.breweryList().forEach(function (brewery) {
+                google.maps.event.addListener(brewery.marker(), 'click', function () {
+                    self.breweryClick(brewery);
+                });
+            });
+        };
+
+        self.breweryClick = function (brewery) {
+            // Infos
+            var infoContent = '<div><h4 id="brewery-name">' + brewery.name() + '</h4>' + '<h5 id="brewery-address">' + brewery.address() + '</h5>' + '<h6 id="brewery-neighborhood">' + brewery.neighborhood() + '</h6>' + '<p id="text">Rating on <a id="yelp-url">yelp</a>: ' + '<img id="yelp"></p></div>';
+            infoWindow.setContent(infoContent);
+            self.getYelpData(brewery);
+
+            // Centraliza Clique
+            map.panTo(new google.maps.LatLng(brewery.lat(), brewery.lng()));
+
+            infoWindow.open(map, brewery.marker());
+
+            self.setMarkerAnimation(brewery);
+        };
+
+        // Bounce
+        self.setMarkerAnimation = function (brewery) {
+            brewery.marker().setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function () {
+                brewery.marker().setAnimation(null);
+            }, 750);
+        };
+
+        // Filtros
+        self.filterBreweries = function () {
+            self.filteredBreweryList([]);
+
+            var searchString = $('#search-str').val().toLowerCase();
+            var len = self.breweryList().length;
+
+            for (var i = 0; i < len; i++) {
+                var breweryName = self.breweryList()[i].name().toLowerCase();
+                var breweryNeighborhood = self.breweryList()[i].neighborhood().toLowerCase();
+
+                if (breweryName.indexOf(searchString) > -1 || breweryNeighborhood.indexOf(searchString) > -1) {
+                    self.filteredBreweryList.push(self.breweryList()[i]);
+                    self.breweryList()[i].marker().setMap(map);
+                } else {
+                    self.breweryList()[i].marker().setMap(null);
+                }
+            }
+        };
+
+        self.getYelpData = function (brewery) {
+
+            var httpMethod = 'GET';
+
+            // Yelp API
+            var yelpURL = 'http://api.yelp.com/v2/search/';
+
+            // Nonce GEN
+            var nonce = function (length) {
+                    var text = "";
+                    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                    for (var i = 0; i < length; i++) {
+                        text += possible.charAt(Math.floor(Math.random() * possible.length));
+                    }
+                    return text;
+                };
+
+            var parameters = {
+                oauth_consumer_key: 'S46AQ1iwQtvxw_D1wQLHZA',
+                oauth_token: 'TO9rPx1abdPe3lllR5Wo3WFrvz8CV9vw',
+                oauth_nonce: nonce(20),
+                oauth_timestamp: Math.floor(Date.now() / 1000),
+                oauth_signature_method: 'HMAC-SHA1',
+                oauth_version: '1.0',
+                callback: 'cb',
+                term: brewery.name(),
+                location: 'São Roque, SP',
+                limit: 1
+            };
+
+            // API's Parametros
+            var consumerSecret = '8hqIHpplfRBLzs6YOqLZFfkx7jg';
+            var tokenSecret = 'evb3bjTox8RNlfZ5Ma74hqJjZWo';
+            var signature = oauthSignature.generate(httpMethod, yelpURL, parameters, consumerSecret, tokenSecret);
+
+            parameters.oauth_signature = signature;
+
+            // Sets do AJAX
+            var ajaxSettings = {
+                url: yelpURL,
+                data: parameters,
+                cache: true,
+                dataType: 'jsonp',
+                success: function (response) {
+                    $('#yelp').attr("src", response.businesses[0].rating_img_url);
+                    $('#yelp-url').attr("href", response.businesses[0].url);
+                },
+                error: function () {
+                    $('#text').html('Data could not be retrieved from yelp.');
+                }
+            };
+
+            $.ajax(ajaxSettings);
+        };
+
+        google.maps.event.addDomListener(window, 'load', function () {
+            self.initialize();
+            self.buildBreweryLocations();
+            self.setBreweryClickFunctions();
+            self.filteredBreweryList(self.breweryList());
+        });
     };
-    map = new google.maps.Map(mapCanvas, mapOptions);
-  };
 
-  self.buildBreweryLocations = function() {
-    breweryLocations.forEach(function(brewItem) {
-      self.breweryList.push( new Brewery(brewItem) );
-    });
-  };
+var Brewery = function (data) {
+        'use strict';
 
-  self.setBreweryClickFunctions = function() {
-    self.breweryList().forEach(function(brewery) {
-      google.maps.event.addListener(brewery.marker(), 'click', function() {
-        self.breweryClick(brewery);
-      });
-    });
-  };
+        var marker;
+        this.name = ko.observable(data.name);
+        this.lat = ko.observable(data.lat);
+        this.lng = ko.observable(data.lng);
+        this.address = ko.observable(data.address);
+        this.neighborhood = ko.observable(data.neighborhood);
 
-  self.breweryClick = function(brewery) {
-    // Infos
-    var infoContent = '<div><h4 id="brewery-name">' + brewery.name() + '</h4>' +
-                      '<h5 id="brewery-address">' + brewery.address() + '</h5>' +
-                      '<h6 id="brewery-neighborhood">' + brewery.neighborhood() + '</h6>' +
-                      '<p id="text">Rating on <a id="yelp-url">yelp</a>: ' +
-                      '<img id="yelp"></p></div>';
-    infoWindow.setContent(infoContent);
-    self.getYelpData(brewery);
+        // Marcador
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(this.lat(), this.lng()),
+            map: map,
+            title: this.name()
+        });
 
-    // Centraliza Clique
-    map.panTo(new google.maps.LatLng(brewery.lat(), brewery.lng()));
-
-    infoWindow.open(map, brewery.marker());
-
-    self.setMarkerAnimation(brewery);
-  };
-
-  // Bounce
-  self.setMarkerAnimation = function(brewery) {
-    brewery.marker().setAnimation(google.maps.Animation.BOUNCE);
-    setTimeout( function() { brewery.marker().setAnimation(null); }, 750);
-  };
-
-  // Filtros
-  self.filterBreweries = function() {
-    self.filteredBreweryList([]);
-
-    var searchString = $('#search-str').val().toLowerCase();
-    var len = self.breweryList().length;
-
-    for (var i = 0; i < len; i++) {
-      var breweryName = self.breweryList()[i].name().toLowerCase();
-      var breweryNeighborhood = self.breweryList()[i].neighborhood().toLowerCase();
-
-      if (breweryName.indexOf(searchString) > -1 ||
-          breweryNeighborhood.indexOf(searchString) > -1) {
-        self.filteredBreweryList.push(self.breweryList()[i]);
-        self.breweryList()[i].marker().setMap(map);
-      } else {
-        self.breweryList()[i].marker().setMap(null);
-      }
-    }
-  };
-
-  self.getYelpData = function(brewery) {
-
-    var httpMethod = 'GET';
-
-    // Yelp API
-    var yelpURL = 'http://api.yelp.com/v2/search/';
-
-    // Nonce GEN
-
-    var nonce = function(length) {
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for(var i = 0; i < length; i++) {
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        return text;
+        this.marker = ko.observable(marker);
     };
 
-    var parameters = {
-      oauth_consumer_key: 'S46AQ1iwQtvxw_D1wQLHZA',
-      oauth_token: 'TO9rPx1abdPe3lllR5Wo3WFrvz8CV9vw',
-      oauth_nonce: nonce(20),
-      oauth_timestamp: Math.floor(Date.now() / 1000),
-      oauth_signature_method: 'HMAC-SHA1',
-      oauth_version: '1.0',
-      callback: 'cb',
-      term: brewery.name(),
-      location: 'São Roque, SP',
-      limit: 1
-    };
-
-    // API's Parametros
-    var consumerSecret = '8hqIHpplfRBLzs6YOqLZFfkx7jg';
-    var tokenSecret = 'evb3bjTox8RNlfZ5Ma74hqJjZWo';
-    var signature = oauthSignature.generate(httpMethod, yelpURL, parameters, consumerSecret, tokenSecret);
-
-    parameters.oauth_signature = signature;
-
-    // Sets do AJAX
-    var ajaxSettings = {
-      url: yelpURL,
-      data: parameters,
-      cache: true,
-      dataType: 'jsonp',
-      success: function(response) {
-        $('#yelp').attr("src", response.businesses[0].rating_img_url);
-        $('#yelp-url').attr("href", response.businesses[0].url);
-      },
-      error: function() {
-        $('#text').html('Data could not be retrieved from yelp.');
-      }
-    };
-
-    $.ajax(ajaxSettings);
-  };
-
-  google.maps.event.addDomListener(window, 'load', function() {
-    self.initialize();
-    self.buildBreweryLocations();
-    self.setBreweryClickFunctions();
-    self.filteredBreweryList(self.breweryList());
-  });
-};
-
-var Brewery = function(data) {
-  'use strict';
-
-  var marker;
-  this.name = ko.observable(data.name);
-  this.lat = ko.observable(data.lat);
-  this.lng = ko.observable(data.lng);
-  this.address = ko.observable(data.address);
-  this.neighborhood = ko.observable(data.neighborhood);
-
-  // Marcador
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng(this.lat(), this.lng()),
-    map: map,
-    title: this.name()
-  });
-
-  this.marker = ko.observable(marker);
-};
-
-ko.applyBindings( new ViewModel() );
+ko.applyBindings(new ViewModel());
--- directbairros/js/model.js
+++ directbairros/js/model.js
@@ -1,149 +1,127 @@
-var breweryLocations = [
-  {
+var breweryLocations = [{
     name: 'Ski Mountain Park',
     lat: -23.506379,
     lng: -47.118271,
     address: 'Av 3 de Maio, s/n° - Cambará, São Roque, SP 18133-310',
     neighborhood: 'Cambará'
-  },
-  {
+}, {
     name: 'Clube Come Together',
     lat: -23.532942,
     lng: -47.133783,
-	address: 'Rua Marechal Deodoro da Fonseca 123-Centro, São Roque, SP 18136-000',
+    address: 'Rua Marechal Deodoro da Fonseca 123-Centro, São Roque, SP 18136-000',
     neighborhood: 'Centro'
-  },
-  {
+}, {
     name: 'Bar do Nelson',
     lat: -23.542647,
     lng: -47.126775,
     address: 'R São Paulo 1204 - Taboão, São Roque, SP 18130-070',
     neighborhood: 'Taboão'
-  },
-  {
+}, {
     name: 'Vinícola Góes',
     lat: -23.611149,
     lng: -47.160335,
     address: 'Km 9 s/n° - Canguera, São Roque, SP 18145-002',
     neighborhood: 'Canguera'
-  },
-  {
+}, {
     name: 'Stefano Hotel e Restaurante',
     lat: -23.551064,
     lng: -47.101483,
     address: 'Av. Antonio Pannellini 2650 - Taboão, São Roque, SP 18135-131',
     neighborhood: 'Taboão'
-  },
-  {
+}, {
     name: 'São Roque Clube',
     lat: -23.530302,
     lng: -47.136847,
     address: 'Av Antonino Dias Bastos 368 - Centro, São Roque, SP 18130-350',
     neighborhood: 'Centro'
-  },
-  {
+}, {
     name: 'Hotel Cordialle',
     lat: -23.534937,
     lng: -47.137435,
     address: 'R Sotero de Souza 500 - Centro, São Roque, SP 18130-200',
     neighborhood: 'Centro'
-  },
-  {
+}, {
     name: 'Morro do Cruzeiro',
     lat: -23.537625,
     lng: -47.143170,
     address: 'R do Cruzeiro s/n° - Jardim Suiça, São Roque, SP 18130-000',
     neighborhood: 'Jd Suiça'
-  },
-  {
-    name: 'V8 Restaurante & Pizzzaria',
-    lat: -23.527720,
-    lng: -47.137283,
-    address: 'Av Antonino Dias Bastos 699 - Centro, São Roque, SP 18130-350',
+}, {
+    name: 'Restaurante Deodoro',
+    lat: -23.522593,
+    lng: -47.132016,
+    address: 'Av Bandeirantes 361 - Centro, São Roque, SP 18134-170',
     neighborhood: 'Centro'
-  },
-  {
+}, {
     name: 'Santa Costela',
     lat: -23.533171,
     lng: -47.136785,
     address: 'Av Antonino Dias Bastos 37 - Centro, São Roque, SP 18130-350',
     neighborhood: 'Centro'
-  },
-  {
+}, {
     name: 'Quinta do Olivardo',
     lat: -23.576726,
     lng: -47.139200,
     address: 'Estrada do Vinho s/n° - Sorocamirim, São Roque, SP 18145-002',
     neighborhood: 'Sorocamirim'
-  },
-  {
+}, {
     name: 'Cantineiro Bar',
     lat: -23.520875,
     lng: -47.138562,
     address: 'R V e Dois - Centro, São Roque, SP 18132-305',
     neighborhood: 'Centro'
-  },
-  {
+}, {
     name: 'O Padroeiro',
     lat: -23.524818,
     lng: -47.133363,
     address: 'Av Antonino Dias Bastos 2-124 - Centro, São Roque, SP 18130-350',
     neighborhood: 'Centro'
-  },
-  {
+}, {
     name: 'Snack House',
     lat: -23.529986,
     lng: -47.134632,
     address: 'R Rui Barbosa n°305 - Centro, São Roque, SP 18130-000',
     neighborhood: 'Centro'
-  },
-  {
+}, {
     name: 'Padaria Martinelli',
     lat: -23.530606,
     lng: -47.135126,
     address: 'Praça da Matriz n°18 - Centro, São Roque, SP 18145-040',
     neighborhood: 'Centro'
-  },
-  {
+}, {
     name: 'Vinícola Palmeiras',
     lat: -23.612101,
     lng: -47.156547,
     address: 'Estrada do Vinho KM10 s/n° - Canguera, São Roque, SP 18130-000',
     neighborhood: 'Canguera'
-  },
-  {
+}, {
     name: 'Cantina da Tia Lina',
     lat: -23.612497,
     lng: -47.155533,
     address: 'Estrada do Vinho KM10,4 s/n° - Canguera, São Roque, SP 18145-002',
     neighborhood: 'Canguera'
-  },
-  {
+}, {
     name: 'Vinhos e Restaurante Frank',
     lat: -23.550045,
     lng: -47.127003,
     address: 'Av. Madre Silva 494 - Vila Amaral, São Roque, SP 18136-280',
     neighborhood: 'Vila Amaral'
-  },
-  {
+}, {
     name: 'Restaurante Paladar Sr',
     lat: -23.531127,
     lng: -47.135137,
     address: 'R. Quinze de Novembro n°42  - Centro, São Roque, SP 18130-000',
     neighborhood: 'Centro'
-  },
-  {
+}, {
     name: 'Churrascaria Chama Sul',
     lat: -23.555491,
     lng: -47.113359,
     address: 'Rod Raposo Tavares 448 - Jd Mosteiro, São Roque, SP 18130-000',
     neighborhood: 'Jardim Mosteiro'
-  },
-  {
+}, {
     name: 'Vinícola Canguera',
     lat: -23.601118,
     lng: -47.162751,
     address: 'Rodovia Prefeito Quintino de Lima - Canguera, São Roque, SP 18130-000',
     neighborhood: 'Canguera'
-  }
-];
+}];
